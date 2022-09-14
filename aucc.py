621d31be35d71fdc40d39c1c1b507b91dc875234
Veli Tekin <33375393+vvelitkn@users.noreply.github.com>
2022-09-14 23:25:57 +0300

    Update aucc.py

--- aucc.py
+++ aucc.py
@@ -1,5 +1,7 @@
 import nmap
 import os
+from string import ascii_lowercase, ascii_lowercase, ascii_lowercase, ascii_uppercase, ascii_uppercase, ascii_uppercase, ascii_letters
+import string
 import sys
 import argparse
 import subprocess
@@ -8,19 +10,22 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen
 from urllib.parse import urlsplit
 import re
+
 ext = set()
-def getExt(url):
+def getInExt(url):
     o = urllib.parse.urlsplit(url)
     html = urlopen(url)
     bs = BeautifulSoup(html, 'html.parser')
-    for link in bs.find_all('a', href = re.compile('^((https://)|(http://))')):
+    for link in bs.find_all('a', href = re.compile('^((https://)|(http://)|())')):
         if 'href' in link.attrs:
             if o.netloc in (link.attrs['href']):
-                continue
+                ext.add(link.attrs['href'])
+            #    continue
             else:
                 ext.add(link.attrs['href'])
 
 def print_current(ip, port):
+
     print ("Target IP: "+ip, end="")
     if (port != -1 ):
         print ("|| Port Range: "+port)
@@ -73,18 +78,67 @@ def nmapscan():
     if (nm[ip_data].has_tcp(80)):
         print ("Perhaps, there is a website, check it. You can inspect source code or fuzz.")
         print('----------------------------------------------------')
-        fuzzdir(ip_data)
+        if (input("If you want to fuzz with gobuster type \"fuzz\"  and press enter\n") == "fuzz"):
+            fuzzdir(ip_data)
+            remove()
+            remove()
+    if (nm[ip_data].has_tcp(21)):
+        print("Check if there is anonymous login FTP")
+    if (nm[ip_data].has_tcp(139) or nm[ip_data].has_tcp(445)):
+        print("Check if there is public samba share")
 
 def fuzzdir(ip_data):    
-    if (input("If you want to fuzz with gobuster type \"fuzz\"  and press enter\n") == "fuzz"):
-        temp = input("Which path(default /): ")
-        print("Running command: gobuster dir -u http://" + ip_data+ temp + " -w /usr/share/wordlists/dirbuster/directory-list-2.3-small.txt -t 150")
-        os.system("gobuster dir -u http://"+ ip_data+ temp +"/ -w /usr/share/wordlists/dirbuster/directory-list-2.3-small.txt -t 100 -q -n -e")
-        
-def exturls():
-    ip_data = input("\nEnter target IP address: \n >> ")
+    remove()
+    remove()
+    print("Which path (default /): ", end="")
+    temp = input()
+    remove()
+    print("Running command: gobuster dir -u http://" + ip_data+ temp + " -w /usr/share/wordlists/dirbuster/directory-list-2.3-small.txt -t 150")
+    os.system("gobuster dir -u http://"+ ip_data+ temp +" -w /usr/share/wordlists/dirbuster/directory-list-2.3-small.txt -t 100 -q -n -e")
+   
+def endecryption():
+    os.system("clear")
+    print("""a) Base64 decryption
+b) Base64 encryption
+c) ROT encryption/encryption
+d) Hash identifier""")
+    choice = input("Your choice: ")
+    os.system("clear")
+    if (choice=="a"): 
+        temp = input("Enter the text: ")
+        os.system("echo "+temp+" | base64 --decode")
+    elif (choice=="b"): 
+        temp = input("Enter the text: ")
+        os.system("echo "+temp+" | base64")
+    elif (choice=="c"): 
+        message = input("Enter the text: ")
+        for key in range (1,25):
+            s = ""
+            upperalphabet = ascii_uppercase
+            loweralphabet = ascii_lowercase
+            for letter in message:
+                if letter in " ~`!@#$%^&*()+_-={}|[]\:;,./<>?":
+                    s = s + letter
+                elif letter in string.ascii_uppercase:
+                    s = s + upperalphabet[(upperalphabet.index(letter) + key) % 26]  # %62 is used to make the list cyclic
+                elif letter in string.ascii_lowercase:
+                    s = s + loweralphabet[(loweralphabet.index(letter) + key) % 26 ]
+                else:
+                    s =s+ letter
+            print(str(key) +". iteration:\t"+s)
 
-    getExt("http://"+ip_data+"/")
+    
+    
+     
+def exturls():
+    os.system("clear")
+    ip_data = input("\nEnter target IP address: \n >> ")
+    print('----------------------------------------------------')
+    print ("Scraping everything...")
+    print('----------------------------------------------------')
+
+    getInExt("http://"+ip_data+"/")
+    
     for i in ext:
         print(i)
     
@@ -100,7 +154,9 @@ def main():
 
     func = input("""Choose the option you need:
     a) Enumerate the target
-    b) Encryption/Decryption\nYour option: """)
+    b) Encryption/Decryption
+    c) Enumarate external/internal links
+    Your option: """)
     if (func == "a"):
         nmapscan()
         
